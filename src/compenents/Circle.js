d9f77abb7a3ff11dc59ed887fe7966f627cf28bf
ninew089 <57795422+ninew089@users.noreply.github.com>
2021-01-18 16:49:08 +0700

    update

--- src/compenents/Circle.js
+++ src/compenents/Circle.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
-
-import { Stage, Layer, Star, Circle } from 'react-konva';
+import { Stage, Layer, Image, Circle } from 'react-konva';
+import useImage from 'use-image';
+import Images from "./Image"
 function generateShapes() {
     return [...Array(50)].map((_, i) => ({
       id: i.toString(),
@@ -12,8 +13,9 @@ function generateShapes() {
 }
 const INITIAL_STATE = generateShapes();
 
-export default function Kodava() {
 
+export default function Kodava() {
+console.log(INITIAL_STATE)
     const [circles, setCircles] = React.useState(INITIAL_STATE);
     const handleDragStart = (e) => {
         const id = e.target.id();
@@ -43,24 +45,24 @@ export default function Kodava() {
       <Stage width={360} height={438}>
       
           <Layer >
-              
-   
+
+        <Images src="http://128.199.244.46:4000/getimage?image=inferNone94323.jpeg" />
               {circles.map((circle) => (
                   <>
                       <Circle
-                           key={circle.id}
-                           id={circle.id}
-          x={circle.x}
-          y={circle.y}
-            draggable
-            radius={10}
-                          fill="yellow"
-                          opacity={0.8}
-                          onDragStart={handleDragStart}
-                          onDragEnd={handleDragEnd}
-                          shadowColor="black"
-                          shadowBlur={10}
-                          shadowOpacity={0.6}
+                            key={circle.id}
+                            id={circle.id}
+                            x={circle.x}
+                            y={circle.y}
+                            draggable
+                            radius={5}
+                            fill="yellow"
+                            opacity={0.8}
+                            onDragStart={handleDragStart}
+                            onDragEnd={handleDragEnd}
+                            shadowColor="black"
+                            shadowBlur={10}
+                            shadowOpacity={0.6}
           />
        
                       </>
