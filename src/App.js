d96e49f009edbdbfd58fd02fc86255b229d3304a
ninew089 <57795422+ninew089@users.noreply.github.com>
2021-01-19 10:18:45 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React,{useState} from 'react';
 import './App.css';
 import Slider from "./compenents/Slider"
 import Kodava from "./compenents/Circle"
-function generateShapes() {
-  return [...Array(50)].map((_, i) => ({
-    id: i.toString(),
-    x: Math.random() * 360,
-    y: Math.random() * 438,
-    weight: (Math.random()*10).toFixed(2),
+import {Button} from '@material-ui/core';
 
-  }));
-}
-const INITIAL_STATE = generateShapes();
+
 
 function App() {
   const [value, setValue] = useState([0.00, 10.00]);
 
-  const [circles, setCircles] = useState(INITIAL_STATE);
+  const [circles, setCircles] = useState([]);
+  function generateShapes() {
+    return setCircles([...Array(50)].map((_, i) => ({
+      id: i.toString(),
+      x: Math.random() * 360,
+      y: Math.random() * 438,
+      weight: (Math.random()*10).toFixed(2),
   
+    })));
+  }
   const data = circles.filter((circle) => {
   return ((parseFloat(circle.weight) >= value[0] && parseFloat(circle.weight) <= value[1])) 
 }
@@ -27,6 +28,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button onClick={generateShapes}>Reset</Button>
         <Kodava circles={data} setCircles={setCircles}/>
         <Slider value={value} setValue={setValue}/>
       </header>
